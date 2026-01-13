970bd4af21e0384a52e81ca819ee97d66989c8b7
kenzz-sz <r4mseiandsei@gmail.com>
2026-01-13 21:02:29 +0700

    update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,11 @@
 (function() {
+  window.refresh = function(){
+ document.getElementById("delaybotsendtxt").innerHTML = settings.delaybotsend;
+  }
   window.opensettingsmenu = function(){
     bot(`
     [ Settings Menu ]<br>
-    > <button class="menubutton" onclick="const v = prompt('delay 1000 = 1s'); if(!isNaN(v)){settings.delaybotsend = v; reload()}">edit</button>
+    > <button class="menubutton" onclick="const v = prompt('delay 1000 = 1s'); if(!Number.isNaN(v)){settings.delaybotsend = v; refresh()}">edit</button>
     `)
   }
   system("clear")
