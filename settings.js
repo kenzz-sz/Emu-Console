d5a96c908cacf45e084cc5c417ae5c669778440c
kenzz-sz <r4mseiandsei@gmail.com>
2026-01-13 20:13:31 +0700

    Expose opensettingsmenu as a global function

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 (function() {
-  function opensettingsmenu(){
+  window.opensettingsmenu = function(){
     t(`
     [ Settings Menu ]<br>
     > <button class="menubutton" onclick="const v = prompt('delay 1000 = 1s'); if(!isNaN(v)){settings.delaybotsend = v}">Set Delay Send</button>
