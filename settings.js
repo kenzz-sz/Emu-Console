4db120c34193728c4571adab03561e057b88f8e7
kenzz-sz <r4mseiandsei@gmail.com>
2026-01-13 20:01:42 +0700

    Refactor settings menu and clear function calls

--- settings.js
+++ settings.js
@@ -1,27 +1,31 @@
 (function() {
   window.opensettingsmenu = function(){
     t(`
-    Settings Menu
-    <button class="menubutton" onclick="const v = prompt('delay 1000 = 1s; if(!isNaN(v)'){settings.delaybotsend = v}">Set Delay Send</button>
+    [ Settings Menu ]<br>
+    > <button class="menubutton" onclick="const v = prompt('delay 1000 = 1s; if(!isNaN(v)'){settings.delaybotsend = v}">Set Delay Send</button>
     `)
+  }
+  function clear(){
+    output = "";
+    reload()
   }
   system("clear")
   t("|")
-  d(500, `system("clear"); t("\\")`)
-  d(1000, `system("clear"); t("—")`)
-  d(1500, `system("clear"); t("|")`)
-  d(2000, `system("clear"); t("\\")`)
-  d(2500, `system("clear"); t("—")`)
-  d(3000, `system("clear"); t("/")`)
-  d(3500, `system("clear"); t("|")`)
-  d(4000, `system("clear"); t("\\")`)
-  d(4500, `system("clear"); t("—")`)
-  d(5000, `system("clear"); t("|")`)
-  d(5500, `system("clear"); t("\\")`)
-  d(6000, `system("clear"); t("—")`)
-  d(6500, `system("clear"); t("/")`)
-  d(7000, `system("clear"); t("|")`)
-  d(7500, `system("clear"); opensettingsmenu()`)
+  d(500, `clear(); t("\\")`)
+  d(1000, `clear(); t("—")`)
+  d(1500, `clear(); t("|")`)
+  d(2000, `clear(); t("\\")`)
+  d(2500, `clear(); t("—")`)
+  d(3000, `clear(); t("/")`)
+  d(3500, `clear(); t("|")`)
+  d(4000, `clear(); t("\\")`)
+  d(4500, `clear(); t("—")`)
+  d(5000, `clear(); t("|")`)
+  d(5500, `clear(); t("\\")`)
+  d(6000, `clear(); t("—")`)
+  d(6500, `clear(); t("/")`)
+  d(7000, `clear(); t("|")`)
+  d(7500, `clear(); opensettingsmenu()`)
 
 
 })();
