b152cd1e08a0a78828213e158024467519adb543
kenzz-sz <r4mseiandsei@gmail.com>
2026-01-13 20:04:20 +0700

    Refactor settings.js to remove clear function
    Removed the clear function and its associated delay animations, replacing it with a direct call to opensettingsmenu.

--- settings.js
+++ settings.js
@@ -5,27 +5,8 @@
     > <button class="menubutton" onclick="const v = prompt('delay 1000 = 1s; if(!isNaN(v)'){settings.delaybotsend = v}">Set Delay Send</button>
     `)
   }
- window.clear = function(){
-    output = "";
-    reload()
-  }
   system("clear")
-  t("|")
-  d(500, `clear(); t("\\")`)
-  d(1000, `clear(); t("—")`)
-  d(1500, `clear(); t("|")`)
-  d(2000, `clear(); t("\\")`)
-  d(2500, `clear(); t("—")`)
-  d(3000, `clear(); t("/")`)
-  d(3500, `clear(); t("|")`)
-  d(4000, `clear(); t("\\")`)
-  d(4500, `clear(); t("—")`)
-  d(5000, `clear(); t("|")`)
-  d(5500, `clear(); t("\\")`)
-  d(6000, `clear(); t("—")`)
-  d(6500, `clear(); t("/")`)
-  d(7000, `clear(); t("|")`)
-  d(7500, `clear(); opensettingsmenu()`)
+  d(500, `opensettingsmenu()`)
 
 
 })();
