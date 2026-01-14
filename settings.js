bdb788b95e9621986290f9df7fbaef8c855f376b
kenzz-sz <r4mseiandsei@gmail.com>
2026-01-14 12:18:13 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,3 +1,12 @@
 (function () {
-  
+
+const datatxtemu = {
+"ui": ""
+}
+ await fetch("https://raw.githubusercontent.com/kenzz-sz/Emu-Console/refs/heads/main/ui-settings.txt")
+  .then(res => res.text())
+  .then(text => datatxtemu.ui = text);
+bot(datatxtemu.ui)
+
+
 })();
