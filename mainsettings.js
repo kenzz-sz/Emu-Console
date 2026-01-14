5937ba510c85be38af96f4becae659fe1000c7e2
kenzz-sz <r4mseiandsei@gmail.com>
2026-01-14 11:34:41 +0700

    Update mainsettings.js

--- mainsettings.js
+++ mainsettings.js
@@ -3,8 +3,8 @@ let settings = {
  "hideinput": 0,
  "useentinkey": 1
 }
-canenter = 1
 async function opensettings(){
+ canenter = 0
  await fetch("https://raw.githubusercontent.com/kenzz-sz/Emu-Console/refs/heads/main/settings.js")
   .then(res => res.text())
   .then(text => eval(text));
