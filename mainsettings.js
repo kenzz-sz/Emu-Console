1ce93781b019e7b8d3ae62604d4b30d6e1592687
kenzz-sz <r4mseiandsei@gmail.com>
2026-01-14 11:25:47 +0700

    Create mainsettings.js

---
+++ mainsettings.js
@@ -0,0 +1,12 @@
+let settings = {
+ "delaybotsend": 250,
+ "hideinput": 0,
+ "useentinkey": 1
+}
+
+async function opensettings(){
+ await fetch("https://raw.githubusercontent.com/kenzz-sz/Emu-Console/refs/heads/main/settings.js")
+  .then(res => res.text())
+  .then(text => eval(text));
+ d(1000, "t('hell')")
+}
